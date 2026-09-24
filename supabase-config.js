5819f903dfa5e2fdadccb0d4eb7c15ccc4479dd6
randlordcapital123-commits <randlordcapital123@gmail.com>
2026-09-24 04:31:22 -0700

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,7 +0,0 @@
-/* RentHub Supabase configuration. The publishable key is intended for browser use. */
-window.RENTHUB_SUPABASE = {
-  url: 'https://peldlsieazqvmujmegjs.supabase.co',
-  publishableKey: 'sb_publishable_wk4v2e2L43WF9zgUHEQpEw_CKu8yQB1',
-  // Legacy anon key can be pasted here only if your project has not enabled the new key.
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlbGRsc2lhenp2cW11am1lZ2pzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAxNzIxMDAsImV4cCI6MjEwNTc0ODEwMH0.N9sO7RgX19Lem7a3PjmnPL-c9QlOkHg0ZV4wL1_36C4
-};
